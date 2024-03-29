8bee2c1ba9afba73259248455d9bae3c80a927cb
HaSYuksel <halisyuksel3@gmail.com>
2024-03-29 15:40:13 -0500

    added all assets

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
-// In your App.js or similar file
+import React from 'react';
+import daySprite from './assets/dayNight.jpeg';
+import temperatureSprite from './assets/temp_emoji.png';
+import pressureSprite from './assets/atmosphere_pressure.webp';
+import windSprite from './assets/wind_emoji.webp';
+
+
+const Sprite = ({ imgSrc, text }) => {
+  const spriteStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: '20px' // Ensure there's some spacing around each sprite
+  };
+
+  return (
+    <div style={spriteStyle}>
+      <img src={imgSrc} alt={text} style={{ width: '100px', height: 'auto' }} />
+      <p>{text}</p>
+    </div>
+  );
+};
+
 
 function App() {
   const style = {
-    background: 'linear-gradient(#051C2C, #893C47,#2E1A47)',
+    background: 'linear-gradient(#051C2C, #893C47, #2E1A47)',
     minHeight: '100vh',
     margin: 0,
     color: 'white',
-    textAlign: 'center' // Added text-align style here
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  };
+
+  const cornerStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '100%'
   };
 
   return (
     <div className="App" style={style}>
-      {/* Your app content */}
+      <div style={cornerStyle}>
+        <Sprite imgSrc={daySprite} text="(Day/Night)" />
+        <Sprite imgSrc={temperatureSprite} text="Temperature 50°F" />
+      </div>
+      <div style={{ flex: 1 }}></div> {/* Spacer div for demonstration */}
+      <div style={cornerStyle}>
+        <Sprite imgSrc={pressureSprite} text="Atmospheric Pressure 200P" />
+        <Sprite imgSrc={windSprite} text="Wind Speed 346MPH" />
+      </div>
     </div>
   );
 }
